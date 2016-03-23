175ff737374e231b68f032ee7f208ce3b94f09f0
Tom Pickering <tom@0xagon.co.uk>
2016-03-23 19:24:36 +0000

    [REF] Simplified regex match-check function

--- re.c
+++ re.c
@@ -30,22 +30,12 @@ bool matches_regex(char *str, char *exp) {
     regex_t re;
     int res;
 
-    if (regcomp(&re, exp, REG_EXTENDED|REG_ICASE|REG_NOSUB|REG_NEWLINE)) {
+    if (regcomp(&re, exp, REG_EXTENDED|REG_ICASE|REG_NOSUB|REG_NEWLINE))
         die("Could not compile search regex.");
-    }
 
     res = regexec(&re, str, 0, NULL, 0);
     regfree(&re);
 
-    switch (res) {
-        case 0:
-            return true;
-        case REG_NOMATCH:
-            return false;
-        default:
-            die("Regex match failed.");
-    }
-
-    /* Never reached, but keeps -Wreturn-type happy */
+    if (!res) return true;
     return false;
 }
